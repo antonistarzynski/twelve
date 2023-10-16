e164eb9efaef2c914e0e70aae0239460c99789f5
Antek Starzynski <antek.starzynski@gmail.com>
2023-10-16 17:29:20 +0200

    add: label buttress

--- labels.js
+++ labels.js
@@ -1,9 +1,16 @@
 const labels = [
+    // {
+    //     name: "Connaisseur",
+    //     pic: 'connaisseur.jpeg',
+    //     short: 'connaisseur.html',
+    //     description: "The Offenbach-based indie-label operating since 2005 just turned 10 years in the business offering a special series of parties in occasion of its anniversary. London, Berlin, Amsterdam have been already some of the successful stop of the tour and a perfect reason to keep on going with this special offer. Write us to know more about Connaisseur label night in your club."
+    // },
     {
-        name: "Connaisseur",
-        pic: 'connaisseur.jpeg',
-        short: 'connaisseur.html',
-        description: "The Offenbach-based indie-label operating since 2005 just turned 10 years in the business offering a special series of parties in occasion of its anniversary. London, Berlin, Amsterdam have been already some of the successful stop of the tour and a perfect reason to keep on going with this special offer. Write us to know more about Connaisseur label night in your club."
+        name: "Buttress",
+        pic: 'buttress.jpeg',
+        short: 'buttress.html',
+        description: `Buttress is a record label which aims to give a solid contribution to djs, dancers and listeners of electronic music.
+        Buttress is committed to supporting artists and their unique expressions, promoting an inclusive and vibrant community, and continually pushing the boundaries of what electronic music can be.`
     },
     {
         name: "Engrave Ltd",
